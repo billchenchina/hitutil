2fee5fec63578c72d04d4781fc8d3a6442781561
billchenchina <billchenchina2001@gmail.com>
2021-01-17 20:59:48 +0800

    Dump version 0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='hitutil',
-    version='0.0.1',
+    version='0.0.2',
     description='hit.edu.cn utils',
     long_description=long_description,
     long_description_content_type='text/markdown',
