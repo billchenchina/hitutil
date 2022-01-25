1b7d20539d38d689504bfdd07052942392aef627
Commiter16 <75511620+Commiter16@users.noreply.github.com>
2022-01-25 22:50:14 +0800

    feat: login system update (#4)
    * feat: login system update
    
    Login system update, modify codes to fit changes.
    
    * Update hit/ids/login.py
    
    * Update login.py
    
    * Fix soup range
    
    * Add NotImplementedError to idslogin
    
    Co-authored-by: Commiter16 <75511620+Commiter16@users.noreply.github.com>

--- hit/ids/login.py
+++ hit/ids/login.py
@@ -9,6 +9,9 @@ from requests import Session
 
 from .utils import encrypt, rds
 
+import time
+import json
+
 
 def idslogin(username: str, password: str, **kwargs) -> Session:
     """Handle ids.hit.edu.cn login
@@ -32,40 +35,41 @@ def idslogin(username: str, password: str, **kwargs) -> Session:
     else:
         s = Session()
     # get pwdDefaultEncryptSalt
-    r1 = s.get('http://ids.hit.edu.cn/authserver/login')
-    pwd_default_encrypt_salt = re.compile(
-        'pwdDefaultEncryptSalt = "(.*)"').search(r1.text).groups()[0]
+    r1 = s.get('https://ids.hit.edu.cn/authserver/login')
+    soup = BeautifulSoup(r1.text, 'html.parser').select_one('#pwdFromId')
+    pwd_default_encrypt_salt = soup.find('input', {'id': 'pwdEncryptSalt'})['value']
     passwordEncrypt = encrypt(
         rds(64).encode()+password.encode(), pwd_default_encrypt_salt.encode())
     # Detect Captcha
-    r2 = s.get('http://ids.hit.edu.cn/authserver/needCaptcha.html',
+    r2 = s.get('https://ids.hit.edu.cn/authserver/checkNeedCaptcha.htl',
                params={
                    'username': username,
-                   'pwdEncrypt2': 'pwdEncryptSalt'
+                   '_': round(time.time() * 1000)
                })
-    if r2.text == 'true':
-        if 'captchaResponse' in kwargs:
-            captchaResponse = kwargs['captchaResponse']
-        else:
-            r = s.get('http://ids.hit.edu.cn/authserver/captcha.html', params={
-                'ts': random.randint(0, 999)
-            })
-            raise CaptchaNeeded(s, r.content)
+    if json.loads(r2.text)['isNeed']:
+        raise NotImplementedError('Captcha is unsupported currently.')
+        # if 'captchaResponse' in kwargs:
+        #     captchaResponse = kwargs['captchaResponse']
+        # else:
+        #     r = s.get('http://ids.hit.edu.cn/authserver/captcha.html', params={
+        #         'ts': random.randint(0, 999)
+        #     })
+        #     raise CaptchaNeeded(s, r.content)
     else:
         captchaResponse = None
-    soup = BeautifulSoup(r1.text, 'html.parser')
     r = s.post(r1.url, data={
         "username": username,
         "password": passwordEncrypt,
-        "captchaResponse": captchaResponse,
+        "captcha": captchaResponse,
         "lt": soup.find('input', {'name': 'lt'})['value'],
         "dllt": soup.find('input', {'name': 'dllt'})['value'],
         "execution": soup.find('input', {'name': 'execution'})['value'],
         "_eventId": soup.find('input', {'name': '_eventId'})['value'],
-        "rmShown": soup.find('input', {'name': 'rmShown'})['value'],
+        "cllt": soup.find('input', {'name': 'cllt'})['value'],
         # "pwdDefaultEncryptSalt": pwd_default_encrypt_salt
     })
-    if r.url != 'http://ids.hit.edu.cn/authserver/index.do':
+    if r.url not in ['https://ids.hit.edu.cn/personalInfo/personCenter/index.html', 
+                     'https://ids.hit.edu.cn/personalInfo/personalMobile/index.html']:
         raise LoginFailed()
 
     if kwargs.get('need_check_resp', False):
