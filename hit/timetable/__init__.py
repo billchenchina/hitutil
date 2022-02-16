bd2c6c426ff31041c7df642d86d2bd502c5494a7
inclyc <axoford@icloud.com>
2022-02-16 15:10:54 +0800

    update 'timetable' to satisfy reviewer-constraints

--- hit/timetable/__init__.py
+++ hit/timetable/__init__.py
@@ -3,10 +3,12 @@ import json
 import pandas as pd
 
 
-def query_by_id(s: requests.sessions.Session, hit_id: str, semester: str, weekday: int):
+def query_by_id(gxh: str, semester: str, weekday: int, s: requests.sessions.Session=None):
+    if s is None:
+        s = requests.Session()
     r = requests.post(url='https://wxfwdt.hit.edu.cn/app/bkskbcx/kbcxapp/getBkszkb', data={
         'info': json.dumps({
-            'gxh': hit_id,
+            'gxh': gxh,
             'zc': str(weekday),
             'xnxq': semester,
         })
